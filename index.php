cba808068b441e5705996893def8d71eb7ae80cf
Oscar Martinez <pumasmau13@gmail.com>
2021-03-03 22:55:58 -0600

    ADD: se agrega el archivo index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+	echo "Presentacion personal";
+?>
+
+<body bgcolor="#AF7AC5">
+<br />
+
+<head>
+	<h1>!Hola! me llamo Oscar Mauricio Martínez Rodríguez</h2>
+</head>
+<br />
+<body>
+	<p>
+	Actualmente estudio la licenciatura en informatica en la Facultad de Contaduria y administracion,
+	pienso en hacer mi servicio social en las ramas informaticas de seguridad o forense, ya que a eso 
+	me gustaria dedicarme en un futuro. Me emociona formar parte de un equipo informatico dedicado a
+	defender los datos de los usuarios de los ciberdelincuentes.
+	</p>
+
+	<p>
+	Por otro lado independiente, estudio musica ya que me apaciona y me gusta desestresarme al tocar,
+	y continuando con el tema, estoy aprendiendo teclado, practico guitarra y canto por mi cuenta.
+	</p>
+
+	<p>
+	Tambien ultimamente eh colaborado y entre a una banda de rock indie "THE BALTERS" donde ya estrene
+	mi primer cancion cantada por mi con ellos, desde octubre salio en youtube y ahora esta disponible,
+	en todas las plataformas digitales como spotify, entre otras.
+	</p>
+
+	<p>
+	En este semestre aparte de materias tambien meti un taller de  dibujo ya que tambien es un arte en
+	el que me siento muy comodo, y pues me relaja demasiado el dibujar y darle forma a las cosas
+	dentro de mi cabeza.
+	</p>
+
+	<p>
+	Ya por ultimo estoy tambien concursando por una beca en Santander sobre programacion web,
+	es un curso para que salgas certificado como programador web full stack JR, y la verdad
+	quiero ganar un lugar
+	</p> 
+<body/>
+
